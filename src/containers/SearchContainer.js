92ddc501bc9d429fd6238d31293b17f40c9f59c4
Lisa Shepard <lisashepard04@gmail.com>
2018-07-17 01:01:03 -0400

    add more search stuff

--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -9,33 +9,45 @@ class SearchContainer extends Component {
     super();
     this.state = {
       input: '',
-      selectedMovie: ''
+      movies: {}
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSearch = this.handleSearch.bind(this);
   }
   
+  async componentDidMount() {
+    console.log('component did mount state', this.state);
+  }
+  
+  // async componentDidUpdate() {
+  //   let input = this.state.input;
+  //   let req = `https://api.themoviedb.org/3/movie/${input}?&api_key=6832af0b5adf0f6be66a81304f9fe064`;
+  //   let res = await axios(req);
+  //   let data = res.data;
+  //   this.setState({ movieId: data.id, data: data });
+  // }
+
+  // componentDidUpdate(nextProps, nextState) {
+  //   if (nextState.keyword.length && nextState.keyword !== this.state.keyword) {
+  //     this.movies(nextState.keyword, res => (
+  //       this.setState({ movies: res })
+  //     ));
+  //   }
+  // }
+
   handleChange(event) {
+    console.log('the event', event.target);
     event.preventDefault();
     this.setState({ input: event.target.value });
   }
 
-  // async componentDidUpdate(prevProps) {
-  //   let query = this.state.input;
-  //   if (this.props.input !== prevProps.input) {
-  //     let res = await axios(`https://api.themoviedb.org/3/search/movie?include_adult=false&page=1&language=en-US&api_key=6832af0b5adf0f6be66a81304f9fe064&query=${query}`);
-  //     let data = res.data;
-  //     this.setState({ selectedMovie: data });
-  //   }
-  // }
-
-  async handleSearch() {
+  async handleSearch(event) {
     let query = this.state.input;
     let res = await axios(`https://api.themoviedb.org/3/search/movie?include_adult=false&page=1&language=en-US&api_key=6832af0b5adf0f6be66a81304f9fe064&query=${query}`);
     let data = res.data;
-    this.setState({ selectedMovie: data });
+    this.setState({ movies: data });
+    console.log('the response', data);
   }
-
 
   render() {
     const input = this.state.input;
@@ -44,9 +56,10 @@ class SearchContainer extends Component {
       <div>
         <Search
           handleChange={this.handleChange}
-          handleSearch={this.handleSearch}
           input={input}
+          getMovie={this.getMovie}
         />
+        <button onClick={this.handleSearch}>Search</button>
       </div>
     );
   }
